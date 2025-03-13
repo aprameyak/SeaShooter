6f20860034191bfdf33df7cf24240b06219824bf
Aprameya Kannan <101409453+aprameyak@users.noreply.github.com>
2025-03-12 22:17:37 -0400

    Update GameStatus.java

--- src/game/GameStatus.java
+++ src/game/GameStatus.java
@@ -2,6 +2,17 @@ package game;
 
 import java.awt.Graphics;
 
+/**
+ * This interface is used for GameStatus
+ * It is used as anonymous class to 
+ * display the status of the game
+ */
 public interface GameStatus {
+	/**
+	 * Displays the status on the game
+	 * 
+	 * @param status
+	 * @param g
+	 */
 	 void gameStatus(String status, Graphics g);
 }
