5106d53e8cc0a3eebcf45530272871e30e168fdd
Aprameya Kannan <101409453+aprameyak@users.noreply.github.com>
2025-03-12 22:19:17 -0400

    Update Enemy.java

--- src/game/Enemy.java
+++ src/game/Enemy.java
@@ -1,8 +1,35 @@
 package game;
 
+/**
+ * This interface is used for Enemy
+ * It is the target for submarine
+ * and it attacks submarine
+ */
 interface Enemy {
+
+    /**
+     * Attack submarine
+     */
     void attack();
+
+    /**
+     * Takes damage based on damage
+     * 
+     * @param damage
+     */
     void takesDamage(int damage);
+
+    /**
+     * Returns the attack damage
+     * 
+     * @return
+     */
     int getAttackDamage();
+
+    /**
+     * Changes the speed to speed
+     * 
+     * @param speed
+     */
     void changeSpeed(double speed);
 }
