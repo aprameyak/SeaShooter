93d3e7c7dac752c595aa120adf3b6a8c5c0befca
Aprameya Kannan <101409453+aprameyak@users.noreply.github.com>
2025-03-12 22:20:27 -0400

    Update Damageable.java

--- src/game/Damageable.java
+++ src/game/Damageable.java
@@ -1,7 +1,18 @@
 package game;
 @FunctionalInterface
+
+/**
+ * This is the interface for Damageable
+ * It represents an object that can 
+ * be damaged
+ */
 public interface Damageable {
 	
-	    void applyDamage(int damage);
-	}
+	/**
+	 * Applies the damage based on damage
+	 * 
+	 * @param damage
+	 */
+	void applyDamage(int damage);
+}
 
